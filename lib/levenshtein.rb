4e947d2a9cd7478779126085bb98b1ace8d47198
Andrew King <me@andrewsking.com>
2014-08-02 14:09:59 +1000

    Memoize levenshtein calculations for speed

--- lib/levenshtein.rb
+++ lib/levenshtein.rb
@@ -1,16 +1,22 @@
 class String
   def levenshtein comparable
     raise ArgumentError, 'Argument is not a string' unless comparable.is_a? String
+    @levenshtein_cache = {}
     levenshtein_step self, comparable
   end
 
   def levenshtein_step first_string, second_string
+    index = [first_string, second_string].sort.join
+    return @levenshtein_cache[index] ||= calculate_distance(first_string, second_string)
+  end
+
+  def calculate_distance first_string, second_string
     return second_string.length if first_string.empty?
     return first_string.length if second_string.empty?
 
     cost = first_string[-1] == second_string[-1] ? 0 : 1
-    return [levenshtein_step(first_string.chop,   second_string     ) + 1,
-            levenshtein_step(first_string,        second_string.chop) + 1,
-            levenshtein_step(first_string.chop,   second_string.chop) + cost  ].min
+    return [ levenshtein_step(first_string.chop,   second_string     ) + 1,
+             levenshtein_step(first_string,        second_string.chop) + 1,
+             levenshtein_step(first_string.chop,   second_string.chop) + cost  ].min
   end
 end
